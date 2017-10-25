3d186f9768d77964a1d5d546578dc13b508a310e
Guest User <Guest@epicodus-50.local>
2017-10-25 11:55:26 -0700

    adds create action in nation controller

--- CivilizationReplica/Controllers/NationController.cs
+++ CivilizationReplica/Controllers/NationController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using CivilizationReplica.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,15 +13,28 @@ namespace CivilizationReplica.Controllers
 {
     public class NationController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<User> _userManager;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
         public IActionResult Create()
         {
             return View();
         }
-        [HttpPost]
-        public IActionResult CreateNation(Nation nation)
-        {
 
-            return View();
+        [HttpPost, ActionName("Create")]
+        public async Task<IActionResult> CreateNation(Nation nation)
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var currentUser = await _userManager.FindByIdAsync(userId);
+            nation.User = currentUser;
+            _db.Nations.Add(nation);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
